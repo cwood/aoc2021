801329e6e56043f03df8b387e1dc6701000e587b
Colin Wood <cwood06@gmail.com>
2021-12-02 10:07:19 -0800

    Update test name

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -24,7 +24,7 @@ func parseAsInt(t *testing.T, text []byte) []int {
 	return ints
 }
 
-func TestDay1_Part1(t *testing.T) {
+func TestDay1(t *testing.T) {
 	t.Run("Day1 Test Input should be 7", func(t *testing.T) {
 		testInput, err := ioutil.ReadFile("testinput")
 		require.NoError(t, err)
