d6a082db611b3199c2e1b7b389a8ecd52e7716b9
Colin Wood <cwood06@gmail.com>
2021-12-01 19:08:54 -0800

    Day 1 part 2 done

--- day1/day1.go
+++ day1/day1.go
@@ -1,6 +1,32 @@
 package day1
 
-func CalculateIncreases(ints []int) int {
+func createWindows(ints []int, size int) []int {
+	var (
+		windowInts = make([]int, 0)
+		intsLen    = len(ints) - 1
+	)
+
+createWindows:
+	for i, _ := range ints {
+		var windowSize = 0
+		for j := 0; j <= size-1; j++ {
+			index := i + j
+			if index > intsLen {
+				continue createWindows
+			}
+			windowSize += ints[index]
+		}
+		windowInts = append(windowInts, windowSize)
+	}
+
+	return windowInts
+}
+
+func CalculateIncreases(ints []int, windowSize int) int {
+	if windowSize > 1 {
+		ints = createWindows(ints, windowSize)
+	}
+
 	var increases = 0
 	for i, n := range ints {
 		if i > 0 {
--- day1/day1_test.go
+++ day1/day1_test.go
@@ -28,13 +28,26 @@ func TestDay1_Part1(t *testing.T) {
 	t.Run("Day1 Test Input should be 7", func(t *testing.T) {
 		testInput, err := ioutil.ReadFile("testinput")
 		require.NoError(t, err)
-		assert.Equal(t, 7, CalculateIncreases(parseAsInt(t, testInput)))
+		assert.Equal(t, 7, CalculateIncreases(parseAsInt(t, testInput), 1))
 	})
 
 	t.Run("Day1 Input 1", func(t *testing.T) {
 		input, err := ioutil.ReadFile("input")
 		require.NoError(t, err)
 
-		t.Logf("Day 1 Part 1: %d", CalculateIncreases(parseAsInt(t, input)))
+		t.Logf("Day 1 Part 1: %d", CalculateIncreases(parseAsInt(t, input), 1))
+	})
+
+	t.Run("Day1 Test Input Part 2 should be 5", func(t *testing.T) {
+		testInput, err := ioutil.ReadFile("testinput")
+		require.NoError(t, err)
+		assert.Equal(t, 5, CalculateIncreases(parseAsInt(t, testInput), 3))
+	})
+
+	t.Run("Day1 Input Part 2", func(t *testing.T) {
+		input, err := ioutil.ReadFile("input")
+		require.NoError(t, err)
+
+		t.Logf("Day 1 Part 2: %d", CalculateIncreases(parseAsInt(t, input), 3))
 	})
 }
