5885d78329b4c87c64dcc895ed776650ca573152
Colin Wood <cwood06@gmail.com>
2021-12-09 11:30:12 -0800

    Day 9 part 1 done

--- day9/day9.go
+++ day9/day9.go
@@ -7,22 +7,50 @@ var positions = [][]int{
 	{0, -1}, // Left
 }
 
-func genChecks(cp []int) [][]int {
+func genChecks(cp []int, maxRow, maxCol int) [][]int {
+	var poss = make([][]int, 0)
 	for _, p := range positions {
+		gened := []int{
+			p[0] + cp[0],
+			p[1] + cp[1],
+		}
 
+		if gened[0] < 0 || gened[1] < 0 {
+			continue
+		} else if gened[0] > maxRow || gened[1] > maxCol {
+			continue
+		}
+
+		poss = append(poss, gened)
 	}
+
+	return poss
 }
 
 func LowpointInMap(points [][]int) int {
+	var lowPoints = make([]int, 0)
 	for r := 0; r <= len(points)-1; r++ {
+	columnLoop:
 		for c := 0; c <= len(points[r])-1; c++ {
 			pv := points[r][c]
 			if pv == 9 {
 				continue
 			}
 
+			checks := genChecks([]int{r, c}, len(points)-1, len(points[r])-1)
+			for _, c := range checks {
+				spotNum := points[c[0]][c[1]]
+				if spotNum < pv {
+					continue columnLoop
+				}
+			}
+			lowPoints = append(lowPoints, pv)
 		}
 	}
-	return 0
 
+	var pointSumN1 int
+	for _, p := range lowPoints {
+		pointSumN1 += p + 1
+	}
+	return pointSumN1
 }
--- day9/day9_test.go
+++ day9/day9_test.go
@@ -14,4 +14,10 @@ func TestDay9(t *testing.T) {
 		points := parse.MustMap(parse.AsMap(input))
 		assert.Equal(t, 15, LowpointInMap(points))
 	})
+
+	t.Run("Part 1", func(t *testing.T) {
+		input := file.MustLoad(file.LoadAsStrings("input"))
+		points := parse.MustMap(parse.AsMap(input))
+		t.Logf("Part 1: %d", LowpointInMap(points))
+	})
 }
